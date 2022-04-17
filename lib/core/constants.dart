365d0e38a66555dd269f0e0c1a46d30f346c6de2
Moaz <67188321+MeYo0o@users.noreply.github.com>
2022-04-17 10:14:02 +0200

    Modify Projects

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -94,15 +94,17 @@ List<Project> kProjects = [
     url: 'https://github.com/MeYo0o/E-Commerce-App',
   ),
   Project(
-    title: "USMS - Organization",
+    title: "School Management System - HR",
     description:
         "Ultimate School Management System - Employer Side , Has 9 Modules ready to serve a school institution and digitalize it's work.",
     url: 'https://github.com/MeYo0o/usms-employee',
   ),
   Project(
-    title: "USMS - Candidate",
+    title: "School Job Search App",
     description:
-        "Ultimate School Management System - Employee Side , can apply to the institution and once verified , can apply for a posted job and track his/her progress.",
+        "Ultimate School Management System for the Candidate , can apply "
+        "to the "
+        "institution and once verified , can apply for a posted job and track his/her progress.",
     url: 'https://github.com/MeYo0o/usms-system',
   ),
   Project(
@@ -114,52 +116,28 @@ List<Project> kProjects = [
   Project(
     title: "Meals App",
     description:
-        "Great way to categorize your meals into multiple variates.",
+        "A Categorized Meal App including (Ingredients + Vegan + Gluten Free) made with Flutter.",
     url: 'https://github.com/MeYo0o/meals_app',
   ),
   Project(
+    title: "BMI Calculator",
+    description: "A BMI Calculator with Flutter.",
+    url: 'https://github.com/MeYo0o/bmi-calculator-flutter',
+  ),
+  Project(
     title: "Shop App",
-    description:
-        "This Shop App is a connection between multiple vendors and clients.",
+    description: "Simple Shopping App made with Flutter.",
     url: 'https://github.com/MeYo0o/shop_app',
   ),
   Project(
-    title: "Great Places App",
-    description:
-        "Share your beautiful moments with others using location based pictures of the places you visit.",
-    url: 'https://github.com/MeYo0o/great_places_app',
-  ),
-  Project(
     title: "Personal Expenses App",
-    description: "Manage your weekly Expenses.",
+    description: "A Weekly Personal Expenses App made with Flutter.",
     url: 'https://github.com/MeYo0o/personal_expenses',
   ),
   Project(
-    title: "Chat App",
-    description: "Simple Chat App to test Firebase Capabilities.",
-    url: 'https://github.com/MeYo0o/chat_app',
-  ),
-  Project(
-    title: "Weather App",
-    description:
-        "Weather app for your current location and also searched locations with beautiful looking UI.",
-    url: 'https://github.com/MeYo0o/Clima-Flutter',
-  ),
-  Project(
-    title: "Ask the Magic 8 Ball",
-    description:
-        "ASk it any Questions and it will give you random answers.",
-    url: 'https://github.com/MeYo0o/magic-8-ball-flutter',
-  ),
-  Project(
-    title: "Dice Decider",
-    description: "Instead of Rock,Paper&Scissors Try this one!.",
-    url: 'https://github.com/MeYo0o/dicee-flutter',
-  ),
-  Project(
-    title: "Quiz App",
-    description: "Simple Quiz App and see your results at the end.",
-    url: 'https://github.com/MeYo0o/quizapp',
+    title: "Great Places",
+    description: "Location Sharing App made with Flutter.",
+    url: 'https://github.com/MeYo0o/great_places_app',
   ),
 ];
 
