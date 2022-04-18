9374c5fb880d8927fbdc297ea1b9f9f694f752b0
Moaz <67188321+MeYo0o@users.noreply.github.com>
2022-04-18 17:57:28 +0200

    modify Projects

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../models/Project.dart';
-
 //User Related Stuff
 const String kUserName = 'Moaz Ahmed';
 const String kJobTitle = 'Flutter Developer';
@@ -41,89 +39,6 @@ const int kYoutubeVideos = 10;
 const int kGithubProjects = 35;
 //Github Starts
 const int kGithubStars = 10;
-
-//Projects
-List<Project> kProjects = [
-  Project(
-    title: "Food Delivery App with Firebase",
-    description:
-        "A food delivery app with firebase backend. with all the features like user registration, login, food item list, cart, order history, payment, and much more.",
-    url: 'https://github.com/MeYo0o/foody/',
-  ),
-  Project(
-    title: "E-Commerce Complete App - Flutter UI",
-    description: "A complete e-commerce app with firebase backend. with all the "
-        "features like user "
-        "registration, login, product item list, cart, order history, "
-        "payment, and much more.",
-    url: 'https://github.com/MeYo0o/E-Commerce-App',
-  ),
-  Project(
-    title: "USMS - Organization",
-    description:
-        "Ultimate School Management System - Employer Side , Has 9 Modules ready to serve a school institution and digitalize it's work.",
-    url: 'https://github.com/MeYo0o/usms-employee',
-  ),
-  Project(
-    title: "USMS - Candidate",
-    description:
-        "Ultimate School Management System - Employee Side , can apply to the institution and once verified , can apply for a posted job and track his/her progress.",
-    url: 'https://github.com/MeYo0o/usms-system',
-  ),
-  Project(
-    title: "Payment Gateway UI",
-    description:
-        "This is only UI for the Payment Gateway App that i'm working on, with Translations and dynamic themes.",
-    url: 'https://github.com/MeYo0o/Payment-Gateway-UI',
-  ),
-  Project(
-    title: "Meals App",
-    description: "Great way to categorize your meals into multiple variates.",
-    url: 'https://github.com/MeYo0o/meals_app',
-  ),
-  Project(
-    title: "Shop App",
-    description: "This Shop App is a connection between multiple vendors and clients.",
-    url: 'https://github.com/MeYo0o/shop_app',
-  ),
-  Project(
-    title: "Great Places App",
-    description:
-        "Share your beautiful moments with others using location based pictures of the places you visit.",
-    url: 'https://github.com/MeYo0o/great_places_app',
-  ),
-  Project(
-    title: "Personal Expenses App",
-    description: "Manage your weekly Expenses.",
-    url: 'https://github.com/MeYo0o/personal_expenses',
-  ),
-  Project(
-    title: "Chat App",
-    description: "Simple Chat App to test Firebase Capabilities.",
-    url: 'https://github.com/MeYo0o/chat_app',
-  ),
-  Project(
-    title: "Weather App",
-    description:
-        "Weather app for your current location and also searched locations with beautiful looking UI.",
-    url: 'https://github.com/MeYo0o/Clima-Flutter',
-  ),
-  Project(
-    title: "Ask the Magic 8 Ball",
-    description: "ASk it any Questions and it will give you random answers.",
-    url: 'https://github.com/MeYo0o/magic-8-ball-flutter',
-  ),
-  Project(
-    title: "Dice Decider",
-    description: "Instead of Rock,Paper&Scissors Try this one!.",
-    url: 'https://github.com/MeYo0o/dicee-flutter',
-  ),
-  Project(
-    title: "Quiz App",
-    description: "Simple Quiz App and see your results at the end.",
-    url: 'https://github.com/MeYo0o/quizapp',
-  ),
-];
 
 //Colors
 //Dark
