19435a18a1d9cba490c33bd876591be15ffe79d0
Moaz Ahmed <justmeyo2025@gmail.com>
2023-09-18 14:51:23 +0300

    update: recommendations.

--- lib/core/recommendations.dart
+++ lib/core/recommendations.dart
@@ -3,6 +3,12 @@ import '../models/Recommendation.dart';
 
 final List<Recommendation> kRecommendations = [
   Recommendation(
+    name: "Houssam S.  🇹🇷",
+    source: "LinkedIn",
+    text:
+        "I highly recommend Moaz as a senior Flutter developer. he possesses an exceptional level of expertise and experience in Flutter app development.",
+  ),
+  Recommendation(
     name: "Angel A.  🇪🇸",
     source: "Fiverr",
     text: "He created a website for me and I'm loving it. I'm looking "
@@ -16,7 +22,8 @@ final List<Recommendation> kRecommendations = [
   Recommendation(
     name: "Michael S.  🇺🇸",
     source: "Fiverr",
-    text: "He helped me with my wordpress plugin and I am very happy with the result",
+    text:
+        "He helped me with my wordpress plugin and I am very happy with the result",
   ),
   Recommendation(
     name: "Maksym Liman.  🇺🇦",
@@ -27,5 +34,11 @@ final List<Recommendation> kRecommendations = [
     name: "Hossam M.  🇪🇬",
     source: "LinkedIn",
     text: "I was happy to work with you and I hope you will be happy too.",
+  ),
+  Recommendation(
+    name: "Basher S.  🇹🇷",
+    source: "LinkedIn",
+    text:
+        "Moaz is not only proficient in creating visually stunning and responsive user interfaces but also excels in optimizing app performance and ensuring smooth user experiences. he is a proactive problem solver who can tackle complex challenges with ease.",
   ),
 ];
