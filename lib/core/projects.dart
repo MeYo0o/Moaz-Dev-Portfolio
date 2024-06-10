b12d5df493a90925fcef0ea9326d06ac4270f86f
Moaz Ahmed <justmeyo2025@gmail.com>
2024-06-11 00:53:46 +0300

    update: Projects.

--- lib/core/projects.dart
+++ lib/core/projects.dart
@@ -38,9 +38,9 @@ List<Project> kProjects = [
     url: 'https://github.com/MeYo0o/allAcademy-production-screenshots',
   ),
   Project(
-    title: "Foody",
+    title: "Meal Monkey",
     description:
-        "A food delivery app with firebase backend. with all the features like user registration, login, food item list, cart, order history, payment, and much more.",
+        "A Restaurant & food delivery app with firebase backend. with all the features like user registration, login, food item list, cart, order history, payment, and much more.",
     url: 'https://github.com/MeYo0o/foody/',
   ),
   Project(
@@ -82,40 +82,8 @@ List<Project> kProjects = [
     url: 'https://github.com/MeYo0o/shop_app',
   ),
   Project(
-    title: "Great Places App",
-    description:
-        "Share your beautiful moments with others using location based pictures of the places you visit.",
-    url: 'https://github.com/MeYo0o/great_places_app',
-  ),
-  Project(
     title: "Personal Expenses App",
     description: "Manage your weekly Expenses.",
     url: 'https://github.com/MeYo0o/personal_expenses',
-  ),
-  Project(
-    title: "Chat App",
-    description: "Simple Chat App to test Firebase Capabilities.",
-    url: 'https://github.com/MeYo0o/chat_app',
-  ),
-  Project(
-    title: "Weather App",
-    description:
-        "Weather app for your current location and also searched locations with beautiful looking UI.",
-    url: 'https://github.com/MeYo0o/Clima-Flutter',
-  ),
-  Project(
-    title: "Ask the Magic 8 Ball",
-    description: "ASk it any Questions and it will give you random answers.",
-    url: 'https://github.com/MeYo0o/magic-8-ball-flutter',
-  ),
-  Project(
-    title: "Dice Decider",
-    description: "Instead of Rock,Paper&Scissors Try this one!.",
-    url: 'https://github.com/MeYo0o/dicee-flutter',
-  ),
-  Project(
-    title: "Quiz App",
-    description: "Simple Quiz App and see your results at the end.",
-    url: 'https://github.com/MeYo0o/quizapp',
   ),
 ];
